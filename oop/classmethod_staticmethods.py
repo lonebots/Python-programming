4f52d935106df0b5b4f9d955bbddb1657816f78c
lonebots <jishnusurajila@gmail.com>
2020-12-21 13:43:41 +0530

    static,regular,class methods

--- oop/classmethod_staticmethods.py
+++ oop/classmethod_staticmethods.py
@@ -1,5 +1,7 @@
 #class method and static methods in python
 
+#the regular method automatically takes in the instance of a class as the first arguement and we can override this
+
 
 class Employee:
     #class varible for upgrading the pay
@@ -7,8 +9,12 @@ class Employee:
     #number of the employee
     num_of_emp = 0
 
+    '''documentation string __doc__ 
+        included using \''' docstring \'''
+        * need to be indended accordinly or error occurs
+    '''
+
     #___init__ method
-    #we are incrementing the employee number in here since this method is called when ever a new employee is created
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
@@ -20,29 +26,54 @@ class Employee:
     def fullname(self):
         return ('{} {}'.format(self.first, self.last))
 
-    #function common to all the class object
-    #self will help in overriding the default class variable value
+   
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)  #increase the pay by 5%
+
+
+    #defining a class method 
+    #we are setting the class as the first arguement of this method
+    @classmethod 
+    def set_raise_amt(cls,amount):
+        cls.raise_amount=amount#changing the class variable value
+
+    #new classmethod for making employee from string
+    @classmethod 
+    def from_string(cls,emp_str):
+        first,last,pay=emp_str.split('-')
+        return cls(first,last,pay)#returns a new employee object for the new values
+
+
+    @staticmethod
+    def is_workday(day):
+        if(day.weekday()==5 or day.weekday()==6):
+            return False
+        else:
+            return True
+
 
 
 #creating an employee object
 emp1 = Employee("testname", "lasttestname", 60000)
 
-print(emp1.pay)
-emp1.apply_raise()
-print(emp1.pay)  #notice the change in pay value
-'''
-when we are trying to access the class variable the first it checks if the 
-variable is present in the object we are refering to and if it is not present in the
-object that we pass in to the class method then it searches the super class from which it
-is inherited !!
-'''
+#changing the employee class's raise amount the value
+Employee.set_raise_amt(1.04)
 
-#accessing the name space of employee 1 (emp1)
-#we can update the raise_amount attribute the emp1 using its own attribute
-print(emp1.__dict__)  #this will not have the raise amount class variable
+#seting an employee from a string of value
+emp2_str='emp2first-emp2last-20000'
+first,last,pay=emp2_str.split('-')
+emp2=Employee(first,last,pay)
 
-#accessing the name space fo the employee class
-print(Employee.__dict__
-      )  #now this will have the raise amount class variable listed in it
+print(emp2.fullname())
+
+#can create another class method for serving the same purpose
+emp3_str='emp3first-emp3last-40000'
+emp3=Employee.from_string(emp3_str)#creating the employee3 instance
+print(emp3.fullname())
+
+
+#using the new static method
+import datetime
+my_date=datetime.date(2000,10,11)
+
+print(Employee.is_workday(my_date))
